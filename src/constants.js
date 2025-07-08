1768efb8e4f533e8bfda92af5e1d38740e3fac08
iancossa <ianrafaelelias@gmail.com>
2025-07-08 16:09:00 +0530

    Updating certs

--- src/constants.js
+++ src/constants.js
@@ -52,14 +52,14 @@ export const SOCIAL_LINKS = {
 
 export const CERTIFICATIONS = [
   {
-    title: 'AWS Certification',
+    title: 'AWS Educate Machine Learning Foundations',
     provider: 'AWS',
     logo: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg',
     url: 'https://www.credly.com/badges/98e0eb84-9033-4770-a0ae-7f1c300e3a32/public_url',
     code: '849df51a-0657-423d-94bf-7cb8eb1a919b'
   },
   {
-    title: 'AWS Certification',
+    title: 'AWS Educate Introduction to Cloud 101',
     provider: 'AWS',
     logo: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg',
     url: 'https://www.credly.com/badges/9cad4e7e-7f1f-43c3-af1c-62581f3daac6/public_url',
@@ -77,5 +77,12 @@ export const CERTIFICATIONS = [
     logo: 'https://avatars.githubusercontent.com/u/28929274?s=200&v=4',
     url: 'https://app.rocketseat.com.br/certificates/849df51a-0657-423d-94bf-7cb8eb1a919b',
     code: '849df51a-0657-423d-94bf-7cb8eb1a919b'
+  },
+  {
+    title: 'Introducing Generative AI with AWS',
+    provider: 'Udacity',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Udacity_logo.svg',
+    url: 'https://www.udacity.com/certificate/e/63a0dec2-42a8-11f0-ba66-6b33901e16f6',
+    code: '63a0dec2-42a8-11f0-ba66-6b33901e16f6'
   }
 ];
